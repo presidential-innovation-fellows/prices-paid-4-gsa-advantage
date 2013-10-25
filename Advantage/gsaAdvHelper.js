114a8e5f52d1d3815c6050e034b0c15cfb83a0cd
John Felleman <john.felleman@gmail.com>
2013-10-25 09:20:40 -0700

    initial working version

--- Advantage/gsaAdvHelper.js
+++ Advantage/gsaAdvHelper.js
@@ -39,10 +39,41 @@ var advantage = {
         searchresult:
         'body div#wrapper div#container-fluid div#main-alt table tbody tr td table#pagination.pagin',
         listings:
-        'div#container-fluid div#main table tbody tr td table tbody tr td table.greybox tbody tr td.greyVertRuleCenter'
+        'div#container-fluid div#main table tbody tr td table ' +
+	'tbody tr td table.greybox tbody tr td.greyVertRuleCenter',
+	searchCriteria: 'div#criteria a',
+	nothing: 'this is so I dont have to deal with commas'
     },
     insertions: {}
 };
+
+function searchMe(query, numResults, resultHandler) {
+  function handleSearchResult(dataFromSearch, status, jqXhr) {
+    if (dataFromSearch === null || typeof dataFromSearch !== 'object') {
+       alert('No results returned.');
+       resultHandler([]);
+    }
+    if (dataFromSearch["status"] &&
+	  (dataFromSearch[0]["status"] == "BadAuthentication")) {
+      alert('Unable to Authenticate. ' +
+	    'Perhaps your session timed-out. Please log in again.');  
+       resultHandler([]);
+    }
+    resultHandler(dataFromSearch);
+  }
+
+  var jsonReqData =  {
+    // data: {
+       p3username: ppSecrets.serviceUsername,
+       p3password: ppSecrets.servicePassword,
+       numRows: numResults,
+       search_string: query,
+       psc_pattern: '*'
+       };
+    // },
+
+  $.getJSON( ppSecrets.ppUrl, jsonReqData, handleSearchResult);
+}
 
 $(document).ready(function() {
     // This is a left menu replacement
@@ -53,6 +84,10 @@ $(document).ready(function() {
         .insertAfter($(advantage.queries.svcsMenu).parent());
     
     // This is an insertion below the sort bar
+
+    /*
+     * For now don't do this
+
     advantage.insertions.sortbar = $(advantage.queries.sortbar);
     advantage.insertions.sortbar.next().clone()
         .attr("id", "pp-insertion")
@@ -62,22 +97,59 @@ $(document).ready(function() {
             + advantage.content.addMeToSortbar2)
         .insertAfter(advantage.insertions.sortbar);
 
+     */
+
+
     // This is an insertion in the search results
-    $(advantage.queries.searchresult).next().clone().attr("class", "search-result")
-        .html(advantage.content.searchResultHeader
-          + advantage.content.searchResultItem1
-          + chrome.extension.getURL("images/jpf.png")
-          + advantage.content.searchResultItem2
-          + advantage.content.searchResultItem1
-          + chrome.extension.getURL("images/robert.png")
-          + advantage.content.searchResultItem2
-          + advantage.content.searchResultItem1
-          + chrome.extension.getURL("images/greg.png")
-          + advantage.content.searchResultItem2
-          + advantage.content.searchResultFooter)
-        .insertAfter( advantage.queries.searchresult);
+    var searchStr = $(advantage.queries.searchCriteria).text();
+    console.log('search for: ' + searchStr);
+    searchMe(searchStr,3, function (data) {
+      var resultsToDisplay = '<table id="criteria-search-result">';
+      resultsToDisplay += '<tr class="search-result-row">';
+      resultsToDisplay += '<td class="search-result-class">';
+      resultsToDisplay += 'Description';
+      resultsToDisplay += '</td>';
+      resultsToDisplay += '<td class="search-result-class">';
+      resultsToDisplay += 'Vendor';
+      resultsToDisplay += '</td>';
+      resultsToDisplay += '<td class="search-result-class">';
+      resultsToDisplay += 'Unit Price';
+      resultsToDisplay += '</td>';
+      resultsToDisplay += '<td class="search-result-class">';
+      resultsToDisplay += 'More info';
+      resultsToDisplay += '</td>';
+      resultsToDisplay += '</tr>';
+      $.each(data, function(index, value) {
+	resultsToDisplay += '<tr class="search-result-row">';
+	resultsToDisplay += '<td class="search-result-class">';
+	resultsToDisplay += value.productDescription;
+	resultsToDisplay += '</td>';
+	resultsToDisplay += '<td class="search-result-class">';
+	resultsToDisplay += value.vendor;
+	resultsToDisplay += '</td>';
+	resultsToDisplay += '<td class="search-result-class">';
+	resultsToDisplay += value.unitPrice;
+	resultsToDisplay += '</td>';
+	resultsToDisplay += '<td class="search-result-class">';
+	resultsToDisplay += 'coming soon';
+	resultsToDisplay += '</td>';
+	resultsToDisplay += '</tr>';
+      });
+      resultsToDisplay += '</table>';
+      console.log(JSON.stringify(data));
+      $(advantage.queries.searchresult).
+	next().
+	clone().
+	attr("class", "search-result").
+	html(advantage.content.searchResultHeader + resultsToDisplay).
+	insertAfter( advantage.queries.searchresult);
+      });
 
     // This is an insertion in to the item display
+    
+
+    /* For now don't do this...
+
     var origPrice, ppListingQ, ppNewListing, ppListingItems, diamond, newDiamond;
     ppListingQ = $(advantage.queries.listings).parent();
     ppNewListing = ppListingQ.clone().attr("id","pp-recommendation");
@@ -87,4 +159,5 @@ $(document).ready(function() {
     diamond = $(ppListingItems.selector + 'span.black10pt font a').first();
     diamond.clone().insertAfter(diamond).children('img').attr("src", chrome.extension.getURL("images/pp-diamond-icon.gif"));
     ppNewListing.insertAfter(ppListingQ);
+    */
 });
