ce56892fba0262e073a0e2370c664785f3a9b51a
John Felleman <john.felleman@gmail.com>
2013-10-27 02:51:56 -0400

    fixed problems in search and display of item

--- Advantage/gsaAdvHelper.js
+++ Advantage/gsaAdvHelper.js
@@ -42,6 +42,8 @@ var advantage = {
         'div#container-fluid div#main table tbody tr td table ' +
 	'tbody tr td table.greybox tbody tr td.greyVertRuleCenter',
 	searchCriteria: 'div#criteria a',
+	itemSearch: '#main table tr td table tr td table tr td table.black8pt tr td table.black8pt tr td table tr td font strong',
+	itemMatch: 'div#main table tr td table tr td table.greybox tr td input[checked="checked"]',
 	nothing: 'this is so I dont have to deal with commas'
     },
     insertions: {}
@@ -146,18 +148,48 @@ $(document).ready(function() {
       });
 
     // This is an insertion in to the item display
-    
 
-    /* For now don't do this...
-
-    var origPrice, ppListingQ, ppNewListing, ppListingItems, diamond, newDiamond;
-    ppListingQ = $(advantage.queries.listings).parent();
-    ppNewListing = ppListingQ.clone().attr("id","pp-recommendation");
-    ppListingItems = ppListingQ.children('td[class!="greyVertRuleCenter"]');
-    origPrice = parseFloat(ppListingItems.children('strong').first().text().substr(1));
-    ppListingItems.children('strong').text('$' + (0.87 * origPrice).toFixed(2));
-    diamond = $(ppListingItems.selector + 'span.black10pt font a').first();
-    diamond.clone().insertAfter(diamond).children('img').attr("src", chrome.extension.getURL("images/pp-diamond-icon.gif"));
-    ppNewListing.insertAfter(ppListingQ);
-    */
+    var listingHeader =
+	$(advantage.queries.itemMatch).parent().parent().parent().children('tr:first-child');
+    var itemQueryStr = $(advantage.queries.itemSearch).text();
+    console.log('search for: ' + itemQueryStr);
+    searchMe(itemQueryStr,3, function (data) {
+      var resultsToDisplay = '<tr class="item-match-row search-result-row">' +
+	  '<td class="item-match-data search-result-data" colspan=9>' +
+	  'Results from searching Prices Paid Portal for '  +
+	  itemQueryStr + '</td></tr>' +
+	  '<tr class="item-match-row search-result-row">' +
+	  '<td class="item-match-data search-result-data" bgcolor="#AAAAFF"></td>' +
+	  '<td valign="middle" bgcolor="#AAAAFF"' +
+	  'class="greyVertRuleCenter">&nbsp;</td>' +
+	  '<td align="right" valign="middle" nowrap="nowrap" bgcolor="#AAAAFF">' +
+	  '<td class="search-result-class">Unit Price</td>' +
+	  '<td valign="middle" bgcolor="#AAAAFF"' +
+	  'class="greyVertRuleCenter">&nbsp;</td>' +
+	  '<td align="right" valign="middle" nowrap="nowrap" bgcolor="#AAAAFF">' +
+	  '<td class="search-result-class">Contractor</td>' +
+	  '<td valign="middle" bgcolor="#AAAAFF"' +
+	  'class="greyVertRuleCenter">&nbsp;</td>' +
+	  '<td align="right" valign="middle" nowrap="nowrap" bgcolor="#AAAAFF">' +
+	  'Description</td>' +
+	  '</tr>';
+      $.each(data, function(index, value) {
+	resultsToDisplay += '<tr class="item-match-row search-result-row">' +
+	  '<td class="item-match-data search-result-data" bgcolor="#AAAAFF"></td>' +
+	  '<td valign="middle" bgcolor="#AAAAFF"' +
+	  'class="greyVertRuleCenter">&nbsp;</td>' +
+	  '<td align="right" valign="middle" nowrap="nowrap" bgcolor="#AAAAFF">' +
+	  '<td class="search-result-class">' + value.unitPrice + '</td>' +
+	  '<td valign="middle" bgcolor="#AAAAFF"' +
+	  'class="greyVertRuleCenter">&nbsp;</td>' +
+	  '<td align="right" valign="middle" nowrap="nowrap" bgcolor="#AAAAFF">' +
+	  '<td class="search-result-class">' + value.vendor + '</td>' +
+	  '<td valign="middle" bgcolor="#AAAAFF"' +
+	  'class="greyVertRuleCenter">&nbsp;</td>' +
+	  '<td align="right" valign="middle" nowrap="nowrap" bgcolor="#AAAAFF">' +
+	  value.productDescription + '</td>' +
+	  '</tr>';
+      });
+    $(resultsToDisplay).insertBefore(listingHeader);
+  });
 });
