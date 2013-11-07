ef38acb59c619abbcb9b63e4eeaca012bebb90d5
John Felleman <john.felleman@gsa.gov>
2013-11-06 23:46:19 -0500

    add a panel for credentials

--- Advantage/gsaAdvHelper.js
+++ Advantage/gsaAdvHelper.js
@@ -10,52 +10,6 @@ var advantage = {
         '<h2>Prices Paid</h2><p>Find data on prices paid for <strong>',
         addMeToSortbar2:
         '</strong>.</p><p>Visit the Prices Paid portal for more info.</p>',
-        searchResultHeader:
-        '<div class="pp-separator">'
-            + '<p>Here are results for similar items from the Prices Paid portal.</p>'
-        + '</div>',
-        searchResultItem1:
-           '<div class="pp-search-result">'
-            + '<table class="pp-search-result">'
-            + '<tr>'
-            + '<td><img src="',
-        searchResultItem2:
-            '" width="100" height="100")></img></td>'
-            + '<td>'
-            + '<p>We are sorry, but the <strong>Prices Paid</strong> portal is unreachable at present. '
-            + 'Please contact Robert Read at the Presidential Innovation Fellowship for assistance.</p>'
-            + '</td></tr></table>'
-        + '</div>',
-        searchResultFooter:
-          '<div class="pp-separator">'
-            + '<p>Here are results from the basic search.</p>'
-        + '</div>',
-        userPppCredsGrabber:
-            '<div class="modal-dialog-canvas">' +
-            '<div class="modal-dialog" id="get-user-creds">' +
-            '<div class="modal-dialog-hdr">' +
-            '<h2  class="modal-dialog" id="get-user-creds-hdr">' +
-            'Login Required' +
-            '</h2>' +
-            '</div>' +
-            '<div class="modal-dialog-body">' +
-            '<p class="modal-dialog">' +
-            'Please enter your credentials for the Prices Paid Portal' +
-            '</p>' +
-            '<p class="modal-dialog">' +
-            'User Name' +
-            '<input type="text" id="ppp-user">user-name</input>' +
-            '</p>' +
-            '<p class="modal-dialog">' +
-            'Password' +
-            '<input type="password" id="ppp-password"></input>' +
-            '</p>' +
-            '<p class="modal-dialog">' +
-            '<input type="button" id="ppp-submit-login">Submit</input>' +
-            '</p>' +
-            '</div>' +
-            '</div>' +
-            '</div>',
         nothing: 'I always forgot to add and remove commas at the end of an object'
     },
     queries: {
@@ -63,23 +17,16 @@ var advantage = {
         'div#sidemenu ul#MenuBar1 li ul li a[href="http://www.gsa.gov/portal/content/105201"]',
         sortbar:
         'div#wrapper div#container div#main table tbody tr td div#sortbarDeptView',
-        searchresult:
-        'body div#wrapper div#container-fluid div#main-alt table tbody tr td table#pagination.pagin',
-        listings:
-        'div#container-fluid div#main table tbody tr td table ' +
-	'tbody tr td table.greybox tbody tr td.greyVertRuleCenter',
-	searchCriteria: 'div#criteria a',
-	itemSearch: '#main table tr td table tr td table tr td table.black8pt tr td table.black8pt tr td table tr td font strong',
-	// itemMatch: 'div#main table tr td table tr td table.greybox tr td input[checked="checked"]',
-	itemMatch: 'div#main table tr td table tr td table.greybox',
-	nothing: 'this is so I dont have to deal with commas'
-    },
-    insertions: {}
+        itemSearch:
+        '#main table tr td table tr td table tr td table.black8pt' +
+                'tr td table.black8pt tr td table tr td font strong',
+        nothing: 'this is so I dont have to deal with commas'
+    }
 };
 
 function getPppCredFromUser() {
-
 }
+
 function searchMe(query, numResults, resultHandler) {
   function handleSearchResult(dataFromSearch, status, jqXhr) {
     if (dataFromSearch === null || typeof dataFromSearch !== 'object') {
@@ -96,99 +43,102 @@ function searchMe(query, numResults, resultHandler) {
   }
 
   var jsonReqData =  {
-    // data: {
        p3username: ppSecrets.serviceUsername,
        p3password: ppSecrets.servicePassword,
        numRows: numResults,
        search_string: query,
        psc_pattern: '*'
        };
-    // },
 
   $.getJSON( ppSecrets.ppUrl, jsonReqData, handleSearchResult);
 }
 
-function addContentBelowSortBar() {
-    advantage.insertions.sortbar = $(advantage.queries.sortbar);
-    advantage.insertions.sortbar.next().clone()
-        .attr("id", "pp-insertion")
-        .html(
-            advantage.content.addMeToSortbar1
-            + advantage.insertions.sortbar.text()
-            + advantage.content.addMeToSortbar2)
-        .insertAfter(advantage.insertions.sortbar);
-}
+var searchQuals = [ "Mfr Part No", "Manufacturer", "Disaster Recovery"];
+var authenticated = false;
 
-function addLeftNavItem() {
-    $(advantage.queries.svcsMenu)
-        .parent().clone().children('a')
-        .attr("href", "http://www.google.com")
-        .text(advantage.content.addMeToSvcsMenu).parent()
-        .insertAfter($(advantage.queries.svcsMenu).parent());
+function onItemCredsLoaded(data) {
+     $(data).insertBefore('div.adv-helper#item-results-expanded');
+     var credentialsBox =
+            $('div.adv-helper#credentials');
+     var expandedResults =
+            $('div.adv-helper#item-results-expanded');
+     var visible =
+            ($(credentialsBox).css('visibility') === 'visible') ||
+            ($(expandedResults).css('visibility') === 'visible');
+     $('button#submit-creds').on('click', function() {
+        authenticated = true;
+        credentialsBox.css({"visibility": "hidden",
+                    "position": "absolute"});
+        expandedResults.css({"visibility": "visible",
+                    "position": "static"});
+     });
+      $('div#item-results-toggler p#toggler').on('click', function() {
+        visible =
+            ($(credentialsBox).css('visibility') === 'visible') ||
+            ($(expandedResults).css('visibility') === 'visible');
+        if (visible) {
+            credentialsBox.css({"visibility": "hidden",
+                            "position": "absolute"});
+            expandedResults.css({"visibility": "hidden",
+                            "position": "absolute"});
+            $(this).text('Click to show.');
+        } else {
+            if (authenticated) {
+                credentialsBox.css({"visibility": "hidden",
+                                "position": "absolute"});
+                expandedResults.css({"visibility": "visible",
+                                "position": "static"});
+            } else {
+                credentialsBox.css({"visibility": "visible",
+                                "position": "static"});
+                expandedResults.css({"visibility": "hidden",
+                                "position": "absolute"});
+            }
+        $(this).text('Click to hide.');
+        }
+  });
 }
-    
-function addToSearchResults() {
-    var searchStr = $(advantage.queries.searchCriteria).text();
-    console.log('search for: ' + searchStr);
-    searchMe(searchStr,3, function (data) {
-      var resultsToDisplay = '<table id="criteria-search-result">';
-      resultsToDisplay += '<tr class="search-result-row">';
-      resultsToDisplay += '<td class="search-result-class">';
-      resultsToDisplay += 'Description';
-      resultsToDisplay += '</td>';
-      resultsToDisplay += '<td class="search-result-class">';
-      resultsToDisplay += 'Vendor';
-      resultsToDisplay += '</td>';
-      resultsToDisplay += '<td class="search-result-class">';
-      resultsToDisplay += 'Unit Price';
-      resultsToDisplay += '</td>';
-      resultsToDisplay += '<td class="search-result-class">';
-      resultsToDisplay += 'More info';
-      resultsToDisplay += '</td>';
-      resultsToDisplay += '</tr>';
-      $.each(data, function(index, value) {
-	resultsToDisplay += '<tr class="search-result-row">';
-	resultsToDisplay += '<td class="search-result-class">';
-	resultsToDisplay += value.productDescription;
-	resultsToDisplay += '</td>';
-	resultsToDisplay += '<td class="search-result-class">';
-	resultsToDisplay += value.vendor;
-	resultsToDisplay += '</td>';
-	resultsToDisplay += '<td class="search-result-class">';
-	resultsToDisplay += value.unitPrice;
-	resultsToDisplay += '</td>';
-	resultsToDisplay += '<td class="search-result-class">';
-	resultsToDisplay += 'coming soon';
-	resultsToDisplay += '</td>';
-	resultsToDisplay += '</tr>';
-      });
-      resultsToDisplay += '</table>';
-      console.log(JSON.stringify(data));
-      $(advantage.queries.searchresult).
-	next().
-	clone().
-	attr("class", "search-result").
-	html(advantage.content.searchResultHeader + resultsToDisplay).
-	insertAfter( advantage.queries.searchresult);
-      });
-}
-
-var templateMap = [
-                {
-                    name: "item",
-                    filename: "gsaAdvItemTemplate.html",
-                    successHandler: onItemTemplateLoaded,
-                    errorHandler: onItemTemplateLoaded
-                }
-            ];
 
 function onItemTemplateLoaded(data) {
-    var resultsToDisplay;
+     executeTemplate(templateMap, 'credentials');
+     var resultsToDisplay;
      $(data).insertBefore('div#sectionheader');
      var itemTitle = $(advantage.queries.itemSearch);
      var itemQueryStr = itemTitle.text();
-     $('div.adv-helper#item-results-expanded p').text('Searched for "' +
-                itemQueryStr + '"');
+     var searchParams =
+        '<thead id="search-params-header">' +
+            '<td>Select</td>' +
+            '<td>Qualifier</td>' +
+            '<td>Value</td>' +
+            '<td></td>' +
+            '<td>Select</td>' +
+            '<td>Custom Field</td>' +
+            '<td>Value</td>' +
+        '</thead>';
+     $.each(searchQuals, function(index, value) {
+        searchParams +=
+            '<tr class="search-result-class">' +
+                '<td><input type="checkbox"></button></td>' +
+                '<td>' +
+                    value +
+                '</td>' +
+                '<td><input type="textfield"></input></td>' +
+                '<td></td>' +
+                '<td><input type="checkbox"></button></td>' +
+                '<td><input type="textfield" placeholder="Custom"></input></td>' +
+                '<td><input type="textfield"></input></td>' +
+            '<tr>';
+     });
+          /*
+          var credentialsBox =
+              $('div.adv-helper#credentials');
+          var expandedResults =
+              $('div.adv-helper#item-results-expanded');
+      */
+     $('table.item-match-table#params tbody').html(searchParams);
+
+     $('div.adv-helper#item-results-expanded p#search-results').
+            text('Searched for "' + itemQueryStr + '"');
      searchMe(buildQueryString(itemQueryStr,
                 retrieveSearchParams()),3, function (data) {
          $.each(data, function(index, value) {
@@ -219,37 +169,6 @@ function onItemTemplateLoaded(data) {
                 $(this).remove();
             });
          });
-          var expandedResults =
-              $('div.adv-helper#item-results-expanded');
-          $('div#item-results-toggler p#toggler').on('click', function() {
-            if ($(expandedResults).css('visibility') === 'visible') {
-                expandedResults.css({"visibility": "hidden",
-                                "position": "absolute"});
-                $(this).text('Click to show.');
-            } else {
-                expandedResults.css({"visibility": "visible",
-                                "position": "static"});
-                $(this).text('Click to hide.');
-            }
-      });
-    });
-}
-
-function onItemTemplateError( jqXHR, textStatus, errorThrown ) {
-    console.log('ajax failed with error: ' + textStatus);
-}
-
-function executeTemplate(templates, templateName) {
-    $.each(templates, function(index, value) {
-        if (value.name === templateName) {
-            $.ajax({
-                type: "GET",
-                url: chrome.extension.getURL(value.filename),
-                dataType: "html"
-                }).done(value.successHandler).
-                    fail(value.errorHandler);
-            return false;
-        }
     });
 }
 
@@ -271,10 +190,39 @@ function retrieveSearchParams() {
     }
     return params;
 }
-/*
 
-         // Handle user actions: open/close portal view, ...
-         */
+var templateMap = [
+                {
+                    name: "item",
+                    filename: "gsaAdvItemTemplate.html",
+                    successHandler: onItemTemplateLoaded,
+                    errorHandler: templateLoadFailed
+                },
+                {
+                    name: "credentials",
+                    filename: "gsaAdvCredsTemplate.html",
+                    successHandler: onItemCredsLoaded,
+                    errorHandler: templateLoadFailed
+                }
+            ];
+
+function templateLoadFailed( jqXHR, textStatus, errorThrown ) {
+    console.log('ajax failed with error: ' + textStatus);
+}
+
+function executeTemplate(templates, templateName) {
+    $.each(templates, function(index, value) {
+        if (value.name === templateName) {
+            $.ajax({
+                type: "GET",
+                url: chrome.extension.getURL(value.filename),
+                dataType: "html"
+                }).done(value.successHandler).
+                    fail(value.errorHandler);
+            return false;
+        }
+    });
+}
 
 $(document).ready(function() {
     
@@ -285,3 +233,28 @@ $(document).ready(function() {
         executeTemplate(templateMap, 'item');
     }
 });
+
+//////////////////////////////////////////////////////////////////////////
+//
+// Functions below this line are not being maintained at the moment
+//
+//////////////////////////////////////////////////////////////////////////
+
+function addContentBelowSortBar() {
+    advantage.insertions.sortbar = $(advantage.queries.sortbar);
+    advantage.insertions.sortbar.next().clone()
+        .attr("id", "pp-insertion")
+        .html(
+            advantage.content.addMeToSortbar1
+            + advantage.insertions.sortbar.text()
+            + advantage.content.addMeToSortbar2)
+        .insertAfter(advantage.insertions.sortbar);
+}
+
+function addLeftNavItem() {
+    $(advantage.queries.svcsMenu)
+        .parent().clone().children('a')
+        .attr("href", "http://www.google.com")
+        .text(advantage.content.addMeToSvcsMenu).parent()
+        .insertAfter($(advantage.queries.svcsMenu).parent());
+}
